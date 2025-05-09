b6deebc70cd416066e3481f2b5eaa0ac305ef18a
willianfrancisco <willian.francisco140@gmail.com>
2025-05-09 09:26:11 -0300

    feat:adicionado documentacao xml aos endpoints veiculo e locatario

---
+++ src/api-service/Adapters/Primary/StartRiderApi/Controllers/LocatarioController.cs
@@ -0,0 +1,62 @@
+using Application.DTOs;
+using Application.Ports;
+using Domain.Entities;
+using Domain.Ports;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StartRiderApi.Controllers
+{
+    [Route("api/locatario")]
+    [ApiController]
+    public class LocatarioController(
+        ILocatarioUseCase _locatarioUseCase,
+        ISerilogLoggerService _logger
+        ) : ControllerBase
+    {
+        /// <summary>
+        /// Cadastra um novo locatario na base de dados.
+        /// </summary>
+        /// <param name="novoLocatario"></param>
+        /// <returns>Status Code 201 Created</returns>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public async Task<IActionResult> CadastrarNovoLocatarioAsync([FromBody]NovoLocatarioDto novoLocatario)
+        {
+            try
+            {
+                await _locatarioUseCase.CadastrarNovoLocatarioAsync(novoLocatario);
+                return Created();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ocorreu um erro no endpoint /POST /api/locatario, mensagem:{ex.Message}");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Atualiza a foto da cnh do locatario
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="atualizaFotoCnhLocatarioDto"></param>
+        /// <returns>Status 204 NoContent</returns>
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarFotoCnhLocatarioAsync([FromRoute] int id, [FromBody] AtualizaFotoCnhLocatarioDto atualizaFotoCnhLocatarioDto)
+        {
+            try
+            {
+                await _locatarioUseCase.AtualizaFotoCnhLocatarioAsync(id, atualizaFotoCnhLocatarioDto);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ocorreu um erro no endpoint /PUT /api/locatario, mensagem:{ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ src/api-service/Adapters/Secondary/Infra/DataMySql/Migrations/20250508190929_MigracaoLocatario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataMySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracaoLocatario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locatarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Cpf = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DataNascimento = table.Column<DateOnly>(type: "date", nullable: false),
+                    NumeroCnh = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DataEmissao = table.Column<DateOnly>(type: "date", nullable: false),
+                    DataValidade = table.Column<DateOnly>(type: "date", nullable: false),
+                    CategoriaCnh = table.Column<int>(type: "int", nullable: false),
+                    FotoCnh = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locatarios", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Locatarios");
+        }
+    }
+}
